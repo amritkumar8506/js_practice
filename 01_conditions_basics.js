e49dab4cc68763c863e1b89e495ef4196dd41bd6
Amrit <amritkumar8410@gmail.com>
2025-08-07 13:40:59 +0530

    Add basic JS practice problems

---
+++ 01_conditions_basics.js
@@ -0,0 +1,116 @@
+// Q1. Write a program that checks whether a number is positive, negative, or zero.
+
+
+// let num = 1;
+// if (num > 0) {
+//     console.log("number is postive");
+
+// } else if (num === 0) {
+//     console.log("number is 0");
+
+// } else {
+//     console.log("number is negative")
+// }
+
+// Q2. Check if a number is even or odd.
+
+// let num = 55;
+// if (num % 2 === 0) {
+//     console.log("number is even");
+
+// } else {
+//     console.log("number is odd");
+
+// }
+
+// Q3. Given a number, check whether it is divisible by both 5 and 11 or not.
+
+// let num = 75;
+// if (num % 5 === 0 && num % 11 === 0) {
+//     console.log("numnber is divisble by 5 and 11 both");
+
+// } else {
+//     console.log(`${num} : is not divisble by 5 and 11`);
+
+// }
+
+// Q4. Take a user's age and check if they are eligible to vote (age ≥ 18).
+
+// let age = 15;
+// if (age < 18) {
+//     console.log("you are not eligble for vote");
+
+// } else {
+//     console.log("you are eligble for vote");
+
+// }
+
+// Q5. Write a program to determine the largest of two numbers.
+
+// let a = 7;
+// let b = 6;
+// if (a > b) {
+//     console.log(`${a} is greater than ${b}`);
+
+// } else {
+//     console.log(`${b} is greater than ${a}`);
+
+// }
+
+// Q6. Check whether a character is a vowel or a consonant.
+
+// let a = "p";
+// if (['a', 'e', 'i', 'o', 'u'].includes(a)) {
+//     console.log("it is a vowel");
+
+// } else {
+//     console.log("it is a consonant");
+
+// }
+
+// Q7. Given a number (1 to 7), print the day of the week using a switch.
+
+
+// switch (day = 6) {
+//     case 1:
+//         day = "Monday";
+//         console.log(`${day}`);
+//         break;
+//     case 2:
+//         day = "Tuesday"
+//         console.log(`${day}`);
+//         break;
+//     case 3:
+//         day = "Wednesday"
+//         console.log(`${day}`);
+
+//         break;
+//     case 4:
+//         day = "Thursday"
+//         console.log(`${day}`);
+//         break;
+//     case 5:
+//         day = "Friday"
+//         console.log(`${day}`);
+//         break;
+//     case 6:
+//         day = "Saturday"
+//         console.log(`${day}`);
+//         break;
+//     case 7:
+//         day = "Sunday"
+//         console.log(`${day}`);
+//         break;
+// }
+
+
+// Q8. Write a program to check whether a year is a leap year or not.
+// let year = 2001;
+// if ((year % 7 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//     console.log("it is a leap year");
+
+// } else {
+//     console.log("it is not a leap year");
+
+// }
+
