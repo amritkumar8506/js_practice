55a694df2add87305c1580741ca8ee87aef4636f
Amrit <amritkumar8410@gmail.com>
2025-08-08 22:01:12 +0530

    Added array practice questions

---
+++ 02_arrays.js
@@ -0,0 +1,60 @@
+// Q1. Add an element "orange" at the end of this array.
+
+
+// let fruits = ["apple", "banana"];
+// fruits.push("orange")
+// console.log(fruits);
+
+
+// Q2. Remove the first element from the array.
+
+
+// let cities = ["Delhi", "Mumbai", "Pune"];
+// cities.shift();
+// console.log(cities);
+
+
+// Q3. Double each number in this array using .map().
+
+// let numbers = [1, 2, 3, 4, 5];
+// let newNo = numbers.map((curElem) => curElem * 2)
+// console.log(newNo);
+
+
+// Q4. Filter out only even numbers from the array.
+
+// let nums = [1, 2, 3, 4, 5, 6];
+// let even = nums.filter((curElem) => {
+//     return curElem % 2 === 0;
+// }
+// )
+// console.log(even);
+
+
+// Q5. Return the first number greater than 10 using .find().
+
+// Q6. Check if all elements are greater than 0 using .every().
+
+// Q7. Check if any number is negative using .some().
+
+// Q8. Convert array of strings to a single string using .join().
+
+// Q9. Sum all numbers in an array using .reduce().
+
+// Q10. Remove "banana" from the array.
+
+// Q11. Return the square of all even numbers in an array.
+
+// Q12. Check if all numbers in the array are greater than 10.
+
+// Q13. Find the first string that starts with "A" (case-sensitive).
+
+// Q14. Create a new array with only the lengths of each word.
+
+// Q15. Sum all odd numbers in an array using .reduce().
+
+// Q16. Check if at least one number is divisible by 7.
+
+// Q17. Sort an array of strings by their length in ascending order.
+
+// Q18. Remove all falsy values from an array using .filter().
