b478c7d3829b1aeea2259643a2de86560b93fc9c
Amrit <amritkumar8410@gmail.com>
2025-09-04 12:02:00 +0530

    Added Question in string

---
+++ 02_string.js
@@ -0,0 +1,13 @@
+//Q1) Reverse a string without using split, reverse, or join.
+
+// 👉 Input: "hello" → Output: "olleh"
+
+// let str = "hello";
+// let reverse = "";
+// let end = str.length - 1;
+// for (let i = end; i >= 0; i--) {
+//     reverse += str[i]
+
+// }
+// console.log(reverse);
+
