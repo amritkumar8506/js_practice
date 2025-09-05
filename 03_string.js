b78569a4a42a264c33fdbd1461326fb16f9a1704
Amrit <amritkumar8410@gmail.com>
2025-09-05 13:14:22 +0530

    Added Question

--- 03_string.js
+++ 03_string.js
@@ -1,6 +1,6 @@
 //?Q1) Reverse a string without using split, reverse, or join.
 
-// 👉 Input: "hello" → Output: "olleh"
+//? 👉 Input: "hello" → Output: "olleh"
 
 // let str = "hello";
 // let reverse = "";
@@ -14,7 +14,7 @@
 
 //? Q2) Reverse each word in a string without changing word order.
 
-//     👉 Input: `"Hello World"` → Output: `"olleH dlroW"`
+//?     👉 Input: `"Hello World"` → Output: `"olleH dlroW"`
 
 // let str = "Hello World";
 // let reverse = ""
@@ -34,6 +34,23 @@
 
 // }
 // console.log(reverse);
+
+
+//? Q3)Count the number of words in a sentence.  
+
+//?👉 Input: `"Hello World"` → Output: `2 Words`
+
+let str = "Hello World"
+// let newStr = str.split(" ")
+let count = 0;
+// console.log(newStr);
+str.forEach(() => {
+
+    count += 1;
+})
+console.log(count);
+
+
 
 
 
